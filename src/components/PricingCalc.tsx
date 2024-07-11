bb9fb0ea6c7b8b049fe2ab6cfca4b09bfacd1a80
MalaviyaIsha <isha.malaviya@exhibytesolution.com>
2024-07-11 10:59:58 +0530

    change

--- src/components/PricingCalc.tsx
+++ src/components/PricingCalc.tsx
@@ -6,7 +6,7 @@ const PricingCalculator = () => {
       <div className="flex flex-col w-full">
         <div className="flex flex-col m-6 justify-center sm:text-[30px] text-[25px] font-bold text-[#0d2c8a] underline">
           <p>Hello world</p>
-          <p>Good morning</p>
+          <p>Good morninggg</p>
         </div>
       </div>
     </div>
